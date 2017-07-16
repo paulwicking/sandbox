4ecc3bd264a14327001be3abd6bf8215f9dda757
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-16 15:21:27 +0200

    Moved logging configuration to json file.

--- python_logging/__init__.py
+++ python_logging/__init__.py
@@ -1,30 +1,21 @@
 from os import path, remove
 import logging
 import logging.config
+import json
 
 from .first_class import FirstClass
 from .second_class import SecondClass
+
 
 #  Delete existing log file
 if path.isfile("logs/python_logging.log"):
     remove("logs/python_logging.log")
 
-#  Create the Logger
+#  Configure logging based on config file
+with open("python_logging_config.json", 'r') as logging_config_file:
+    config_dict = json.load(logging_config_file)
+logging.config.dictConfig(config_dict)
+
+#  Log that the logger is configured
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-#  Create handler for logging to file
-logger_handler = logging.FileHandler('logs/python_logging.log')
-logger_handler.setLevel(logging.INFO)
-
-#  Create formatter for formatting log messages
-logger_formatter = logging.Formatter(
-    fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-#  Add formatter to handler
-logger_handler.setFormatter(logger_formatter)
-
-#  Add Handler to Logger
-logger.addHandler(logger_handler)
 logger.info('logger() configuration complete.')
