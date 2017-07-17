775cab8ec0815dfe248fd744bccc1f1d89173314
Paul <wowsuchnamaste@users.noreply.github.com>
2017-07-17 17:35:29 +0200

    Started effort to remove hard-coded paths.
    The log file location as defined by the .json file containing
    configuration settings dict, is hardcoded (and has to be, because json).

--- logging/python_logging/__init__.py
+++ logging/python_logging/__init__.py
@@ -11,15 +11,20 @@ import json
 from .first_class import FirstClass
 from .second_class import SecondClass
 
+PACKAGE_DIRECTORY = path.dirname(path.abspath(__file__))
+LOG_FILE = PACKAGE_DIRECTORY + r"/../logs/python_logging.log"
+
 
 #  Delete existing log file
-if path.isfile("logs/python_logging.log"):
-    remove("logs/python_logging.log")
+if path.isfile(LOG_FILE):
+    remove(LOG_FILE)
+
 
 #  Configure logging based on config file
-with open("python_logging_config.json", 'r') as logging_config_file:
+with open(PACKAGE_DIRECTORY + "/../config/python_logging_config.json", 'r') as logging_config_file:
     config_dict = json.load(logging_config_file)
 logging.config.dictConfig(config_dict)
+#logging.basicConfig(filename=LOG_FILE)
 
 #  Log that the logger is configured
 logger = logging.getLogger(__name__)
