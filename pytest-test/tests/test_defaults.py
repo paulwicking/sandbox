8072bfeda947d95b3519b36ab16eab813afe1d7a
Paul <wowsuchnamaste@users.noreply.github.com>
2017-07-21 15:57:09 +0200

    More tests.

---
+++ pytest-test/tests/test_defaults.py
@@ -0,0 +1,12 @@
+from tasks import *
+
+def test_defaults():
+    t1 = Task()
+    t2 = Task(None, None, False, None)
+    assert t1 == t2
+
+def test_member_access():
+    t = Task('buy milk', 'brian')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brian'
+    assert (t.done, t.id) == (False, None)
