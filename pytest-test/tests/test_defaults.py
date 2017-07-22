7a9e98f3586a3b0874338046ba81bd5195f04586
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-22 14:56:35 +0200

    Tests now running as expected.

--- pytest-test/tests/test_defaults.py
+++ pytest-test/tests/test_defaults.py
@@ -1,12 +1,12 @@
-from tasks import *
+from tasks import tasks 
 
 def test_defaults():
-    t1 = Task()
-    t2 = Task(None, None, False, None)
+    t1 = tasks.Task()
+    t2 = tasks.Task(None, None, False, None)
     assert t1 == t2
 
 def test_member_access():
-    t = Task('buy milk', 'brian')
+    t = tasks.Task('buy milk', 'brian')
     assert t.summary == 'buy milk'
     assert t.owner == 'brian'
     assert (t.done, t.id) == (False, None)
